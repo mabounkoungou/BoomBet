d8dc5cccb13f7c412be79e96f9ae693dd70ba07c
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2023-10-11 23:04:53 +0000

    Update edit_premium.php

--- admin/edit_premium.php
+++ admin/edit_premium.php
@@ -1,11 +1,5 @@
 <?php
-$host = "sql301.infinityfree.com";
-$username = "if0_35122151_boombet";
-$password = "rrbfKXLmBIJI";
-$database = "if0_35211532_boombet";
-
-$con = mysqli_connect($host, $username, $password, $database);
-
+include 'connection.php';
 
 
 $League = "";
